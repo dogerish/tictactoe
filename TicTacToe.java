75d8dd0ff0ae34a49988dcfc99faca7732cb565b
Theo-Reeves <54045165+Theo-Reeves@users.noreply.github.com>
2022-03-11 12:03:07 -0500

    indentation

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,7 +5,7 @@ public class TicTacToe
     public static Board board = new Board(3, 3);
     public static void main(String[] args)
     {
-		// Swing calls must be run by the event dispatching thread.
-		SwingUtilities.invokeLater(() -> new GUI(board));
+		  // Swing calls must be run by the event dispatching thread.
+		  SwingUtilities.invokeLater(() -> new GUI(board));
     }
 }
